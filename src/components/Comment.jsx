fdc03391ac32c5cef2df04e80a40bcde9867bb61
VanessaMaia26 <vanessamaia2607@gmail.com>
2024-02-06 16:43:33 -0300

    structure

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+import { Avatar } from './Avatar';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/vanessaamaia.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Vanessa Maia</strong>
+                            <time title="03 de Fevereiro ÀS 18:26H" datetime="2024-02-03">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Post.jsx
@@ -0,0 +1,52 @@
+import { Avatar } from './Avatar'
+import { Comment } from './Comment'
+import styles from './Post.module.css'
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <Avatar src="https://github.com/vanessaamaia.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Vanessa Maia</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title="03 de Fevereiro ÀS 18:26H" datetime="2024-02-03">Publicado há 1h</time>
+
+            </header>
+
+            <div className={styles.content}>
+            <p>Fala galeraa 👋</p>
+            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+            <p>👉 {' '} <a href="#">jane.design/doctorcare</a></p>
+            <p>
+                <a href="#">#novoprojeto</a> {' '}
+                <a href="#">#nlw</a> {' '}
+                <a href="#">#rocketseat</a>
+            </p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea
+                    placeholder='Deixe um comentário'
+                />
+
+                <footer>
+                  <button type='submit'>Publicar</button>
+                </footer>
+            </form>
+
+            <div className={styles.commentList}>
+                <Comment />
+                <Comment />
+                <Comment />
+            </div>
+        </article>
+    )
+}
